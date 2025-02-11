aec4140fd0cd5fc59469aac78f3999c38a871de2
berreys <saberrey@gmail.com>
2025-02-11 16:04:27 -0700

    add map functionality to home page

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,12 +1,16 @@
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import MapView from 'react-native-maps';
 
 const HomeScreen = ({ navigation }) => {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Home Screen here</Text>
-            <Button onPress={() => navigation.navigate('Account')} title="Go to account"/>
-            <Icon name="rocket" />
+        // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        //     <Text>Home Screen here</Text>
+        //     <Button onPress={() => navigation.navigate('Account')} title="Go to account"/>
+        //     <Icon name="rocket" />
+        // </View>
+        <View style={{flex:1}}>
+            <MapView style={StyleSheet.absoluteFillObject} showsUserLocation={true} showsMyLocationButton={true} />
         </View>
     );
 }
