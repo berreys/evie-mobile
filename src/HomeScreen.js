2c9025fd4c513aa48024f93372ebf7ebe9fd29a2
berreys <saberrey@gmail.com>
2025-02-14 16:35:47 -0700

    focus map on user by default

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,8 +1,33 @@
 import { Button, Text, View, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import MapView from 'react-native-maps';
+import React, {useState, useEffect} from 'react';
+import * as Location from 'expo-location';
 
 const HomeScreen = ({ navigation }) => {
+    const [location, setLocation] = useState(null);
+    const [region, setRegion] = useState(null);
+    const [locationError, setLocationError] = useState('');
+
+    useEffect(() => {
+        const getLocation = async () => {
+            let {status} = await Location.requestForegroundPermissionsAsync();
+            if(status !== 'granted'){
+                setLocationError('Permission to access location was denied.');
+                return;
+            }
+            let currentLocation = await Location.getCurrentPositionAsync({});
+            setLocation(currentLocation.coords);
+            setRegion({
+                latitude: currentLocation.coords.latitude,
+                longitude: currentLocation.coords.longitude,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005
+            });
+        };
+        getLocation();
+    }, []);
+
     return (
         // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         //     <Text>Home Screen here</Text>
@@ -10,7 +35,7 @@ const HomeScreen = ({ navigation }) => {
         //     <Icon name="rocket" />
         // </View>
         <View style={{flex:1}}>
-            <MapView style={StyleSheet.absoluteFillObject} showsUserLocation={true} showsMyLocationButton={true} />
+            <MapView style={StyleSheet.absoluteFillObject} showsUserLocation={true} showsMyLocationButton={true} initialRegion={region} />
         </View>
     );
 }
