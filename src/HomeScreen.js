9df81953740a361b7617ee99735cd34ddae7937f
berreys <saberrey@gmail.com>
2025-02-20 17:45:26 -0700

    fix initial location getter bug, add marker to user's location

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -5,45 +5,46 @@ import React, {useState, useEffect} from 'react';
 import * as Location from 'expo-location';
 
 const HomeScreen = ({ navigation }) => {
-    const [location, setLocation] = useState();
-    const [region, setRegion] = useState();
+    const [location, setLocation] = useState(null);
+    const [region, setRegion] = useState(null);
     const [locationError, setLocationError] = useState('');
 
-    useEffect(() => {
+    useEffect( () => {
         const getLocation = async () => {
+            console.log("HERE");
             let {status} = await Location.requestForegroundPermissionsAsync();
             if(status !== 'granted'){
+                console.log('No location permission');
                 setLocationError('Permission to access location was denied.');
-                return;
             }
-            let currentLocation = await Location.getCurrentPositionAsync({});
-            setLocation(currentLocation.coords);
-            setRegion({
-                latitude: currentLocation.coords.latitude,
-                longitude: currentLocation.coords.longitude,
-                latitudeDelta: 0.005,
-                longitudeDelta: 0.005
-            });
+            else{
+                let currentLocation = await Location.getCurrentPositionAsync({});
+                setLocation(currentLocation.coords);
+                setRegion({
+                    latitude: currentLocation.coords.latitude,
+                    longitude: currentLocation.coords.longitude,
+                    latitudeDelta: 0.005,
+                    longitudeDelta: 0.005
+                });
+            }
         };
         getLocation();
     }, []);
 
-    const markerTest = {
+    const markerTest = location ? {
         lat: location.latitude,
         long: location.longitude,
         title: 'Your Location'
-    }
+    } : {}
 
     return (
         <View style={{flex:1}}>
             {region ? (
                 <MapView style={StyleSheet.absoluteFillObject} showsUserLocation={true} showsMyLocationButton={true} initialRegion={region}>
-                    {location && (
-                        <Marker
-                            coordinate={{latitude: markerTest.lat, longitude: markerTest.long}}
-                            title={markerTest.title}
-                        />
-                    )}
+                    <Marker
+                        coordinate={{latitude: markerTest.lat, longitude: markerTest.long}}
+                        title={markerTest.title}
+                    />
                 </MapView>
             ) : (
                 <Text style={{textAlign: 'center', marginTop: 20}}>
